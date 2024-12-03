af6f4df5f5a6f66db21973a5d9e9aaebbfc7144b
peach4812 <thanatat.bunn@bumail.net>
2024-12-03 15:37:36 +0700

    abc

---
+++ Assets/Exam 2/spell.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void cast()
+    {
+        Console.WriteLine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
